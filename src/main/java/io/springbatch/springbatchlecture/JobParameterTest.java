988edef6505c1892fd2cc7a5e22ecd42d6f8dd6e
jason07289 <jason07999@naver.com>
2025-03-14 17:34:40 +0900

    db

--- src/main/java/io/springbatch/springbatchlecture/JobParameterTest.java
+++ src/main/java/io/springbatch/springbatchlecture/JobParameterTest.java
@@ -26,6 +26,7 @@ public class JobParameterTest implements ApplicationRunner {
         JobParameters jobParameters = new JobParametersBuilder().addString("name", "user1")
                 .addLong("seq", 1L)
                 .addDate("date", new Date())
+                .addDouble("age", 16.5)
                 .toJobParameters();
 
         jobLauncher.run(job, jobParameters);
